3674997c86f4efb0985145e9779b85ce51773d09
LAM HANG YI <leannelam@LAMs-MacBook-Air.local>
2022-08-14 19:03:52 +0800

    changed style and added prompt

--- script.js
+++ script.js
@@ -4,26 +4,31 @@ const containerNo_div = document.querySelector('.containerNoHead');
 const header_div = document.getElementsByTagName('header');
 const leftside_div = document.querySelector('#leftside');
 containerNo_div.setAttribute('style', 'display: flex; gap: 300px')
-container_div.setAttribute('style', 'display: inline-grid; color: black; background: white; border: solid; grid-template-columns: repeat(16, 1fr);  grid-template-rows: repeat(16, 1fr); width: 500px; height: 500px;');
+container_div.setAttribute('style', 'display: inline-grid; background: white; border: solid; border-color: white; width: 500px; height: 500px;');
+
 
 let isMouseDown = false;
 window.onmousedown = () => (isMouseDown = true);
 window.onmouseup = () => (isMouseDown = false);
 
 function generateGrid() {
-    for (let i = 0; i < 256; i++) {
+    const gridSize = prompt("What size you want?")
+    container_div.style.gridTemplateColumns =  `repeat(${gridSize}, 1fr)`;
+    container_div.style.gridTemplateRows =  `repeat(${gridSize}, 1fr)`;
+
+    for (let i = 0; i < (gridSize ** 2); i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('gridItem');
         container_div.appendChild(gridItem);
-        gridItem.setAttribute('style','border: 1px solid; border-color: black;')
+        gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F;')
         //color changed when mouse onclick the squares
         gridItem.addEventListener('mousedown', () => {
-            gridItem.setAttribute('style','border: 1px solid; border-color: black; background-color: black;');
+            gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: #94B49F;');
         }) ;
 
         gridItem.addEventListener('mouseover', (e) => {
             if (isMouseDown) {
-                gridItem.setAttribute('style','border: 1px solid; border-color: black; background-color: black;');
+                gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: #94B49F;');
             }
         });
     }
@@ -36,9 +41,5 @@ generateGrid();
 
 
 // leftside
-leftside_div.setAttribute('style','width: 300px; border: solid; border-color: black; margin-left: 200px;')
-
-
-
-
+leftside_div.setAttribute('style','width: 300px; border: solid; border-color: #94B49F; margin-left: 200px;')
 
