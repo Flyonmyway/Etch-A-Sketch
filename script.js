80f6b7d24e155a6111e0227a9a6d03d2087fc9ca
LAM HANG YI <leannelam@LAMs-MacBook-Air.local>
2022-08-14 22:24:58 +0800

    Added clear all and eraser

--- script.js
+++ script.js
@@ -5,16 +5,21 @@ const header_div = document.getElementsByTagName('header');
 const leftside_div = document.querySelector('#leftside');
 containerNo_div.setAttribute('style', 'display: flex; gap: 300px')
 container_div.setAttribute('style', 'display: inline-grid; background: white; border: solid; border-color: white; width: 500px; height: 500px;');
-
+leftside_div.setAttribute('style','width: 300px; border: solid; border-color: #94B49F; margin-left: 200px;')
+const clear_btn = document.querySelector('.clear');
+const eraser_btn = document.querySelector('.eraser');
 
 let isMouseDown = false;
 window.onmousedown = () => (isMouseDown = true);
 window.onmouseup = () => (isMouseDown = false);
 
-function generateGrid() {
+function main() {
     const gridSize = prompt("What size you want?")
     container_div.style.gridTemplateColumns =  `repeat(${gridSize}, 1fr)`;
     container_div.style.gridTemplateRows =  `repeat(${gridSize}, 1fr)`;
+    if (gridSize > 100) {
+        alert("Too large");
+    }
 
     for (let i = 0; i < (gridSize ** 2); i++) {
         const gridItem = document.createElement('div');
@@ -26,20 +31,30 @@ function generateGrid() {
             gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: #94B49F;');
         }) ;
 
-        gridItem.addEventListener('mouseover', (e) => {
+        gridItem.addEventListener('mouseover', () => {
             if (isMouseDown) {
                 gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: #94B49F;');
             }
         });
+        //eraser
+        eraser_btn.addEventListener('click', () => {
+            gridItem.addEventListener('mousedown', () => {
+                gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: none;');
+            }) ;
+    
+            gridItem.addEventListener('mouseover', () => {
+                if (isMouseDown) {
+                    gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: none;');
+                }
+            });
+        });
+        
+        //clear  all
+        clear_btn.addEventListener('click', () => {
+            gridItem.setAttribute('style','border: 1px solid; border-color: #94B49F; background-color: none;');
+         });
     }
-}      
+}
 
-generateGrid();
-
-
-
-
-
-// leftside
-leftside_div.setAttribute('style','width: 300px; border: solid; border-color: #94B49F; margin-left: 200px;')
+main();
 
