37b18cf48e3ec30baea38954af0f283ce6e929c7
LAM HANG YI <leannelam@LAMs-MacBook-Air.local>
2022-08-14 17:40:47 +0800

    colour applied when clicking grid and drag

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ const leftside_div = document.querySelector('#leftside');
 containerNo_div.setAttribute('style', 'display: flex; gap: 300px')
 container_div.setAttribute('style', 'display: inline-grid; color: black; background: white; border: solid; grid-template-columns: repeat(16, 1fr);  grid-template-rows: repeat(16, 1fr); width: 500px; height: 500px;');
 
+let isMouseDown = false;
+window.onmousedown = () => (isMouseDown = true);
+window.onmouseup = () => (isMouseDown = false);
 
-for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('gridItem');
-    gridItem.textContent = ' ';
-    container_div.appendChild(gridItem);
-    gridItem.setAttribute('style','border: 1px solid; border-color: black;')
-    //color changed when mouse onclick the squares
-    gridItem.addEventListener('click', () => {
-        gridItem.setAttribute('style','border: 1px solid; border-color: black; background-color: black;');
-    })
-}
+function generateGrid() {
+    for (let i = 0; i < 256; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        container_div.appendChild(gridItem);
+        gridItem.setAttribute('style','border: 1px solid; border-color: black;')
+        //color changed when mouse onclick the squares
+        gridItem.addEventListener('mousedown', () => {
+            gridItem.setAttribute('style','border: 1px solid; border-color: black; background-color: black;');
+        }) ;
+
+        gridItem.addEventListener('mouseover', (e) => {
+            if (isMouseDown) {
+                gridItem.setAttribute('style','border: 1px solid; border-color: black; background-color: black;');
+            }
+        });
+    }
+}      
+
+generateGrid();
+
+
+
 
 
 // leftside
